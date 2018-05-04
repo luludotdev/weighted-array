664e420893423dc9f7080cdcb85c2432fb9a513d
Jack Baron <jackmbaron@gmail.com>
2018-05-04 19:21:43 +0100

    Test for immediate duplicates in selectUnique()

--- test/tests.js
+++ test/tests.js
@@ -7,8 +7,9 @@ const { select, selectUnique } = require('../src/index.js')
 const errors = require('../src/errors.js')
 
 // Test Data
-let checkValue = (expected, actual, margin) => (Math.abs(actual - expected) / expected) < margin
-const testData = [{ value: 'A', weight: 1 }, { value: 'B', weight: 3 }]
+const checkValue = (expected, actual, margin) => (Math.abs(actual - expected) / expected) < margin
+const testData1 = [{ value: 'A', weight: 1 }, { value: 'B', weight: 3 }]
+const testData2 = [{ value: 'A', weight: 1 }, { value: 'B', weight: 2 }, { value: 'C', weight: 3 }]
 
 describe('select()', () => {
   it('should be a function', () => {
@@ -17,7 +18,7 @@ describe('select()', () => {
 
   describe('input types', () => {
     it('should accept an array', () => {
-      expect(() => { select(testData) }).to.not.throw()
+      expect(() => { select(testData1) }).to.not.throw()
     })
     it('should throw when not an array', () => {
       expect(() => { select('string') }).to.throw(errors.inputError)
@@ -43,7 +44,7 @@ describe('select()', () => {
     it('should work with test data', () => {
       let a = 0, b = 0
       for (let i = 0; i < 10000; i++) {
-        let { value } = select(testData)
+        let { value } = select(testData1)
         if (value === 'A') a++
         if (value === 'B') b++
       }
@@ -63,5 +64,22 @@ describe('select()', () => {
 describe('selectUnique()', () => {
   it('should be a function', () => {
     expect(selectUnique).to.be.a('function')
+  })
+
+  it('should produce unique values', () => {
+    let random = selectUnique(testData2)
+    let results = []
+
+    for (let i = 0; i < 500; i++) {
+      let { value } = random.next().value
+      results = [...results, value]
+    }
+
+    let dupes = false
+    for (let i in results) {
+      i = parseInt(i)
+      if (results[i] === results[i + 1]) dupes = true
+    }
+    expect(dupes).to.equal(false)
   })
 })
